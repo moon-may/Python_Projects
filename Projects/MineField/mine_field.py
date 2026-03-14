7c351fec888b06040b938a54b1c3fcc88eb6135e
moon-may <katrin-moonmay@yandex.ru>
2026-03-14 20:35:58 +0300

    Добавлена логка открытия клетки

--- Projects/MineField/mine_field.py
+++ Projects/MineField/mine_field.py
@@ -1,56 +1,92 @@
 import random
 
-# Скрытое от игрока поле
-def create_hidden_field(cols, rows, mines):
-    field = []
+class Game:
 
-    # Задать поле
-    for row in range(rows): # для каждой строки
-        row = []
-        for col in range(cols): # для каждого столбца
-            row.append(0) # добавить 0 в строку
-        field.append(row) # добавить строки в поле
+    def __init__(self, cols, rows, mines):
+        self.cols = cols
+        self.rows = rows
+        self.mines = mines
+        self.game_over = False
 
-    # Разместить мины
-    mines_placed = 0 # размещенные мины
+        self.hidden_field = self.create_hidden_field()
+        self.player_field = [['#'] * self.cols for _ in range(self.rows)]
+        self.flags = [[False for _ in range(cols)] for _ in range(rows)]
 
-    while mines_placed < mines:
-        r = random.randint(0, rows - 1) # случайный ряд
-        c = random.randint(0, cols - 1) # случайный столбец
+    # Скрытое от игрока поле
+    def create_hidden_field(self):
+        # поле, заполненное нулями
+        field = [[0] * self.cols for _ in range(self.rows)]
 
-        # проверка, что мины там нет и добавление
-        if field[r][c] != -1:
-            field[r][c] = -1
-            mines_placed += 1
+        # Разместить мины
+        mines_placed = 0 # размещенные мины
 
-            # Разместить числа
-            # определение соседей
+        while mines_placed < self.mines:
+            r = random.randint(0, self.rows - 1) # случайный ряд
+            c = random.randint(0, self.cols - 1) # случайный столбец
+
+            # проверка, что мины там нет и добавление
+            if field[r][c] != -1:
+                field[r][c] = -1
+                mines_placed += 1
+
+                # Разместить числа
+                # определение соседей
+                for i in range(-1, 2):
+                    for j in range(-1, 2):
+                        # пропустить центр
+                        if i == 0 and j == 0:
+                            continue
+                        # соседи
+                        nr = r + i
+                        nc = c + j
+                        # проверка границ
+                        if 0 <= nr < self.rows and 0 <= nc < self.cols:
+                            # если клетка - не мина
+                            if field[nr][nc] != -1:
+                                field[nr][nc] += 1
+
+        return field
+    
+    # Открыть клетку
+    def open_cell(self, row, col):
+        '''
+        проверь границы
+        проверь, стоит ли флаг, если стоит - не открывай, попроси другие координаты или сменить флаг
+        проверь, открыта ли клетка. если открыта - выведи запрос на другие координаты
+        если закрыта и мина - закончить игру, показать поле
+        если закрыта и цифра - выведи цифру
+        если закрыта и 0 - самовызов на соседние клетки
+        '''
+        if not(0 <= row < self.rows and 0 <= col < self.cols):
+            return 'INVALID_COORDS'     # координаты не существуют
+        elif self.flags[row][col]:
+            return 'HAS_FLAG'           # уже стоит флаг
+        elif self.player_field[row][col] != '#':
+            return 'ALREADY_OPEN'       # уже открыта клетка
+        elif self.hidden_field[row][col] == -1:
+            self.game_over = True       
+            return 'MINE'               # в клетке мина
+        
+        cell_value = self.hidden_field[row][col]
+        self.player_field[row][col] = str(self.hidden_field[row][col])
+
+        if cell_value == 0:
             for i in range(-1, 2):
                 for j in range(-1, 2):
-                    # пропустить центр
                     if i == 0 and j == 0:
                         continue
-                    # соседи
-                    nr = r + i
-                    nc = c + j
-                    # проверка границ
-                    if 0 <= nr < rows and 0 <= nc < cols:
-                        # если клетка - не мина
-                        if field[nr][nc] != -1:
-                            field[nr][nc] += 1
-
-    return field
+                    nr = row + i
+                    nc = col + j
+                    if 0 <= nr < self.rows and 0 <= nc < self.cols:
+                        if self.player_field[nr][nc] == '#' and not self.flags[nr][nc]:
+                            self.open_cell(nr, nc)
 
 
-# Поле игрока
-def create_user_field(cols, rows):
-    user_field = []
-
-    for row in range(rows):
-        row = []
-        for col in range(cols):
-            row.append('#')
-        user_field.append(row)
+    # Смена флага
+    def toggle_flag(self):
+        pass
 
 
-
+    # Состояние игры
+    def game_status(self):
+        pass
